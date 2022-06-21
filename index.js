28ec3fa1d39239393b83cb5318a35ab03b745103
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-21 12:51:25 +0100

    Create promise function

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-const pTag = document.querySelector('.example');
-const input = document.createElement('input');
+// const pTag = document.querySelector('.example');
+// const input = document.createElement('input');
 
-input.value='iam here!!';
+// input.value='iam here!!';
 
-const Btn = document.createElement('button');
-Btn.textContent ='Click me!';
+// const Btn = document.createElement('button');
+// Btn.textContent ='Click me!';
 
-pTag.replaceWith(input, Btn);
+// pTag.replaceWith(input, Btn);
  
-setTimeout( () => {
-    console.log('eggs');
-console.log('apple');
-},3000);
+// setTimeout( () => {
+//     console.log('eggs');
+// console.log('apple');
+// },3000);
 
 
-console.log('orange');
-console.log('banana');
+// console.log('orange');
+// console.log('banana');
 
 
-const names = ['ali', 'ahmad', 'basir'];
-const greet = names.map(name => `Hello ${name}!`);
-console.log(greet);
+// const names = ['ali', 'ahmad', 'basir'];
+// const greet = names.map(name => `Hello ${name}!`);
+// console.log(greet);
 
-// callback function
-let greeting = name => console.log(`Hii ${name}!`);
-const userInfo = (firstName, lastName, callback) => {
-    const fullName =  `${firstName} ${lastName}`;
-    callback(fullName);
+// // callback function
+// let greeting = name => console.log(`Hii ${name}!`);
+// const userInfo = (firstName, lastName, callback) => {
+//     const fullName =  `${firstName} ${lastName}`;
+//     callback(fullName);
+// }
+
+// userInfo('basir','ahmad', greeting);
+
+function displayValue (value) {
+    document.getElementById('demo').innerHTML = value;
 }
 
-userInfo('basir','ahmad', greeting);
+
+const myPromise  = new Promise((resolve, reject ) => {
+    let x=0;
+    if(x==0){
+        resolve('ok');
+    } else {
+        reject('error');
+    }
+});
+
+myPromise.then(
+    function(error) {console.log(error);},
+    function(value) {console.log(value);}
+);
