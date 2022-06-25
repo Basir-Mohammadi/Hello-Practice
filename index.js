5b7e59a240e031b1c96c58e89fa2727a2c39cfeb
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-25 15:50:19 +0100

    Add fibonacci function

--- index.js
+++ index.js
@@ -92,3 +92,21 @@ async function myMeeting() {
 }
 
 myMeeting();
+
+function fibonacci(n) {
+    let output = [];
+  
+    if (n===1) {
+      output = [0];
+    } else if(n===2) {
+      output = [0,1];
+    } 
+    else {
+      for ( let i = 2; i<n; i++) {
+        output.push(output[output.length-2]+output[output.length-1]);
+      }
+    }
+    return output;
+  }
+
+  fibonacci(5);
